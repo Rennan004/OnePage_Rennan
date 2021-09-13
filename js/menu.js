64d0e4edef01c3c16151aa9041ec740f113f7808
Rennan004 <79432537+Rennan004@users.noreply.github.com>
2021-09-13 15:21:32 +0000

    aa

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,43 @@
       e.preventDefault()
       $('#menu').toggle('slow')
     })
+
+    //Criar uma função para monitorar os cliques nos links
+
+    $('a').click(function(e){
+      e.preventDefault()
+      //Capturar qual é a pagina que deseja abrir
+
+      let url = $(this).attr('href')
+      
+      if(url == 'home'){
+         $('#'+url).show()
+         $('#about').hide()
+         $('#action').hide()
+         $('#contact').hide()
+      }
+      else if(url == 'about'){
+       $('#'+url).show()
+       $('#home').hide()
+       $('#action').hide()
+       $('#contact').hide()
+      }
+      else if(url == 'action'){
+       $('#'+url).show()
+       $('#home').hide()
+       $('#about').hide()
+       $('#contact').hide()
+      }
+      else if(url == 'contact'){
+       $('#'+url).show()
+       $('#home').hide()
+       $('#action').hide()
+       $('#about').hide()
+      }
+
+      
+
+    })
+
+ 
  })
